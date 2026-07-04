21c3aa5be7fd88454ae48c7e8f3cab59aa31c039
Sohail Sajid <sohailsajid79@gmail.com>
2026-07-04 12:02:42 +0100

    remove redundant var support

--- platform/management-groups/variables.tf
+++ platform/management-groups/variables.tf
@@ -12,24 +12,10 @@ variable "enable_telemetry" {
   default = true
 }
 
-variable "intermediate_root_id" {
-  type    = string
-  default = "ssdev"
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9_.()-]{1,90}$", var.intermediate_root_id))
-    error_message = "MG id must be 1-90 chars: letters, numbers, hyphens, underscores, periods, parentheses."
-  }
-}
-
-variable "intermediate_root_display_name" {
-  type        = string
-  description = "Display name of the intermediate root management group."
-  default     = "SS Dev Platform"
-}
-
-
 variable "allowed_locations" {
-  type    = list(string)
-  default = ["uksouth"]
+  type = list(string)
+}
+
+variable "subscription_id" {
+  type = string
 }
